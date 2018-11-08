ae19332b65dcfa3a6c4b31f872e5724e1f22f6bc
xcollantes <xcollantes@users.noreply.github.com>
2018-11-07 22:50:43 -0800

    Add website

--- pylio.py
+++ pylio.py
@@ -1,7 +1,10 @@
 #!/bin/python
 
-# author: Xavier Collantes
+# author: Xavier Collantes (xaviercollantes.me)
 # date: November 7, 2018
 # file: pylio.py
 # purpose: Send SMS text messages via email. 
 
+
+
+# <AMDG/>
